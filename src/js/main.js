2710bf8d5cb81841bbbffc83befabfff9fa35c6a
ShunKUN1182 <2250378@ecc.ac.jp>
2026-04-03 18:15:17 +0900

    feat: 大体のロジックが完成

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,71 @@
 import "../css/style.css";
 
+const stationBtn = document.querySelector("#stationBtn");
+const stationName = document.querySelector("#stationName");
+const stationTrainLine = document.querySelector("#trainLine");
+const trainImg = document.querySelector("#trainImg");
+const stationWrap = document.querySelector("#stationWrap");
+
 const osakaStations = [
     {
         name: "天王寺",
-        trainLine: "御堂筋線",
+        trainLine: "大阪メトロ・御堂筋線/谷町線",
+        img: ["./src/assets/Midosuji_logo.png", "./src/assets/Tanimati_logo.svg"],
     },
     {
         name: "昭和町",
-        trainLine: "御堂筋線",
+        trainLine: "大阪メトロ・御堂筋線",
+        img: ["./src/assets/Midosuji_logo.png"],
     },
     {
         name: "西田辺",
-        trainLine: "御堂筋線",
+        trainLine: "大阪メトロ・御堂筋線",
+        img: ["./src/assets/Midosuji_logo.png"],
     },
     {
         name: "長居",
-        trainLine: "御堂筋線",
+        trainLine: "大阪メトロ・御堂筋線",
+        img: ["./src/assets/Midosuji_logo.png"],
     },
     {
         name: "あびこ",
-        trainLine: "御堂筋線",
+        trainLine: "大阪メトロ・御堂筋線",
+        img: ["./src/assets/Midosuji_logo.png"],
     },
     {
         name: "北花田",
-        trainLine: "御堂筋線",
+        trainLine: "大阪メトロ・御堂筋線",
+        img: ["./src/assets/Midosuji_logo.png"],
     },
 ];
+
+stationBtn.addEventListener("click", () => {
+    const randomNum = Math.floor(Math.random() * osakaStations.length);
+    console.log(randomNum);
+    let inputHTML = "";
+    inputHTML += `
+      <div class="flex gap-[12px]">
+    `;
+    osakaStations[randomNum].img.forEach((e) => {
+        inputHTML += `
+          <img
+            id="trainImg"
+            src="${e}"
+            alt=""
+            class="w-[48px] h-[48px] object-cover my-[12px]"
+          />    
+        `;
+    });
+    inputHTML += `
+      </div>
+    `;
+    inputHTML += `
+      <div class="flex flex-col gap-[4px] justify-center items-center">
+          <h2 id="stationName" class="text-3xl font-bold md:text-4xl">${osakaStations[randomNum].name}</h2>
+          <p id="trainLine" class="text-sm text-gray-500">${osakaStations[randomNum].trainLine}</p>
+      </div>
+  `;
+    stationWrap.innerHTML = inputHTML;
+    // stationName.textContent = osakaStations[randomNum].name;
+    // stationTrainLine.textContent = osakaStations[randomNum].trainLine;
+});
