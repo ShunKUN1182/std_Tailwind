ca7c960c54d8c209c1c5d7b2b22773e2489c2bdf
ShunKUN1182 <2250378@ecc.ac.jp>
2026-04-03 17:26:34 +0900

    feat: tailwindを適用　環境構築

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 import "../css/style.css";
+
+const osakaStations = [
+    {
+        name: "天王寺",
+        trainLine: "御堂筋線",
+    },
+    {
+        name: "昭和町",
+        trainLine: "御堂筋線",
+    },
+    {
+        name: "西田辺",
+        trainLine: "御堂筋線",
+    },
+    {
+        name: "長居",
+        trainLine: "御堂筋線",
+    },
+    {
+        name: "あびこ",
+        trainLine: "御堂筋線",
+    },
+    {
+        name: "北花田",
+        trainLine: "御堂筋線",
+    },
+];
